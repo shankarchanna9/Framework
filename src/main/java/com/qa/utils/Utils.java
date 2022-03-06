ca9353d6ba4471bd5ca502db8c9e8e7dc0dd5f9c
shankarchanna9 <shankarchanna9@github.com>
2022-03-06 18:18:34 +0530

    added take screenshot method in util class

--- src/main/java/com/qa/utils/Utils.java
+++ src/main/java/com/qa/utils/Utils.java
@@ -1,8 +1,13 @@
 package com.qa.utils;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -165,6 +170,15 @@ public class Utils extends base {
 		driver.navigate().refresh();
 	}
 	
+	//****************take Screenshot methods************************//
+	
+public void takeScreenshotAtEndOfTest() throws IOException {
+		
+		TakesScreenshot screenshot = ((TakesScreenshot) driver);
+		File scrFile  = screenshot.getScreenshotAs(OutputType.FILE);
+		String currentDir = System.getProperty("user.dir");
+		FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png"));
+	}
 	
 	//************************ Teardown methods ******************//
 	
